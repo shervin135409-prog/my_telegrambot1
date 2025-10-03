e0693fd1b04b9e49f855b5b58a1b4af1e112fca2
shervin135409-prog <shervin135409@gmail.com>
2025-10-03 14:43:52 -0700

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,22 @@
+import os
+from telebot import TeleBot
+
+# توکن از Environment Variable خونده میشه
+TOKEN = os.getenv("7779922952:AAEQ1gb9RUgJTw0PrnVQfGyazbw_LxX5QIQ")
+
+# اگر توکن خالی بود خطا بده
+if not TOKEN:
+    raise ValueError("توکن ربات پیدا نشد! لطفاً BOT_TOKEN رو در Environment Variable ست کن.")
+
+bot = TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "سلام داداش! ربات روی Render وصل شد 😎")
+
+@bot.message_handler(commands=['help'])
+def help_command(message):
+    bot.reply_to(message, "این ربات آماده کاره! /start رو تست کن.")
+
+# ربات همیشه آنلاین با polling
+bot.polling(none_stop=True, timeout=60)
